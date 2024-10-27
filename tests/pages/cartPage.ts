02aa35f00b6d136ac14af4bb53afdfeff4e53b6f
swappac <swap.cv@gmail.com>
2024-10-27 23:05:09 +0000

    Initial test

---
+++ tests/pages/cartPage.ts
@@ -0,0 +1,16 @@
+import { Page } from "@playwright/test";
+
+export class CartPage {
+
+page: Page;
+
+constructor(page: Page){
+    this.page = page;
+}
+
+async clickOnCart() {
+   // await expect(page.locator('#shopping_cart_container').getByRole('link')).toBeVisible();
+    await this.page.getByRole('link', { name: '1' }).click();
+}
+}
+
